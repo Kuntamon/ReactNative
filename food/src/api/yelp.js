5737d4ce51cabaf28defa3e5c38a47d2bc3d8c5d
Kuntamon <54786878+Kuntamon@users.noreply.github.com>
2022-05-14 02:05:17 +0300

    Update yelp.js

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer Rq0cnbTK10dibD6F1YChMuV4KXb4F1CoqCixT1cHb46AcpXF0RHiTVI4C1HNdM5pF7r-6i6tK1EsHgXWj6JKmrt-CoK2mgpvV4SHHmhKNT05Zj6A18Fib9R7OknlXXYx'
+        Authorization: 'Key that you need'
     }
 });
 
